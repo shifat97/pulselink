38da2abb17af59eeb1f6bd718c0c0151404a0940
shifat97 <shifatbinreza2@gmail.com>
2025-07-22 13:52:03 +0600

    add: footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
     return (
         <main className="font-poppins bg-white min-h-screen 2xl:max-w-[1536px] m-2 2xl:mx-auto">
             <Header />
             <Outlet />
-            <footer>Footer</footer>
+            <Footer />
         </main>
     );
 }
---
+++ src/constants/footerLinks.js
@@ -0,0 +1,13 @@
+const companyLinks = [
+    { name: "Home", to: "/" },
+    { name: "About us", to: "/" },
+    { name: "Contact us", to: "/" },
+    { name: "Privacy policy", to: "/" },
+];
+
+const getInTouch = {
+    mobileNumber: "+1-212-456-7890",
+    email: "greatstackdev@gmail.com",
+};
+
+export { companyLinks, getInTouch };
