d70f5105f76d0aba2a295165d9f55749fef8ab3a
shifat97 <shifatbinreza2@gmail.com>
2025-07-22 21:45:44 +0600

    update: move doctor template to separate file

--- src/components/TopDoctors.jsx
+++ src/components/TopDoctors.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { filterByTopDoctor } from "../constants/api";
 import { ToastContainer, toast } from "../constants/toast";
+import Doctor from "../templates/Doctor";
 
 export default function TopDoctors() {
     const [seeMore, setSeeMore] = useState(false);
@@ -67,33 +68,3 @@ export default function TopDoctors() {
         </section>
     );
 }
-
-const Doctor = ({ doctor }) => {
-    const { image, name, type, isAvailable } = doctor;
-    return (
-        <div className="rounded-xl border-1 border-purple2">
-            <div className="bg-purple1 flex items-end justify-center w-full rounded-tl-xl rounded-tr-xl">
-                <img src={image} alt={name} />
-            </div>
-            <div className="p-14">
-                <p
-                    className={`flex items-center gap-2 mt-0.5 ${
-                        isAvailable ? "text-green-300" : "text-red-300"
-                    }`}
-                >
-                    {" "}
-                    <span
-                        className={`w-[7.25px] h-[7.25px] rounded-full ${
-                            isAvailable ? "bg-green-300" : "bg-red-300"
-                        }`}
-                    ></span>{" "}
-                    {isAvailable ? "Available" : "Not Available"}
-                </p>
-                <h3 className="text-22 text-black1 mt-10 font-medium">
-                    {name}
-                </h3>
-                <p className="text-gray1 mt-2">{type}</p>
-            </div>
-        </div>
-    );
-};
