67217e4a8188302850bdef48a730829a683f6570
shifat97 <shifatbinreza2@gmail.com>
2025-09-17 14:07:10 +0600

    fix: Port number for single doctor endpoint

--- src/constants/api.js
+++ src/constants/api.js
@@ -3,7 +3,7 @@ const filterByTopDoctor = "http://localhost:3000/doctors?isTopDoctor=true";
 const filterByNotTopDoctor = "http://localhost:3000/doctors?isTopDoctor=false";
 
 const getDoctorById = (id) => {
-  return `http://localhost:3000/doctors?id=${id}`;
+  return `http://localhost:5000/doctors?id=${id}`;
 };
 
 const getDoctorByType = (type) => {
